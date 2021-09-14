54cc5aa37c2d546a36a79b7876d2e44db34940df
brysonhavics <87788293+brysonhavics@users.noreply.github.com>
2021-09-14 00:06:53 -0400

    Team, TeamCreate, TeamServices

---
+++ FoseBall.Model/TeamCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoseBall.Model
+{
+    public class TeamCreate
+    {
+        [Required]
+        [MaxLength(40, ErrorMessage = "Please shorten your team name")]
+        public string TeamName { get; set; }
+        public int LeagueId { get; set; }
+        public int Rank { get; set; }
+    }
+}
