1632c2b66f9bd5c12a3dbe6588e15dc6a514c179
John Mai <Maijohn960@gmail.com>
2021-09-14 10:06:24 -0400

    second time

---
+++ FoseBall.Data/International.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoseBall.Data
+{
+    public class International
+    {
+        [Key]
+        public string InterantionalId { get; set; }
+        [Required]
+        public string Coach { get; set; }
+        [Required]
+        public int Ranking { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
