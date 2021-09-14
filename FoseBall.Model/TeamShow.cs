b15f77e78b805c56bef2bec6fc6888516e0ada3e
brysonhavics <87788293+brysonhavics@users.noreply.github.com>
2021-09-14 15:51:56 -0400

    Controller added

---
+++ FoseBall.Model/TeamShow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoseBall.Model
+{
+    public class TeamShow
+    {
+        [Required]
+        public string TeamName { get; set; }
+        public int LeagueId { get; set; }
+        public int Rank { get; set; }
+    }
+}
