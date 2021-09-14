839b9a62f5f4ad082174d44ce0819421f74e42c4
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-14 14:25:54 -0400

    get player by Id and player detail model

--- Foseball.Services/PlayerService.cs
+++ Foseball.Services/PlayerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FoseBall.Data;
+using FoseBall.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace Foseball.Services
 {
-    class PlayerServices
+    public class PlayerService
     {
+        public PlayerService() { }
+
+        public bool CreatePlayer(PlayerCreate model)
+        {
+            var entity = new Player() { Name = model.Name, Number = model.Number, NationalityId = model.InternationalId, Position = model.Position, TeamId = model.TeamId };
+
+            using (var ctx = new FoseBallDbContext())
+            {
+                ctx.Players.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public PlayerDetail GetPlayerById(int id)
+        {
+            using(var ctx = new FoseBallDbContext())
+            {
+                var entity = ctx.Players.Single(e => e.Id == id);
+
+                return new PlayerDetail
+                {
+                    Name = entity.Name,
+                    Number = entity.Number,
+                    Position = entity.Position,
+                    TeamId = entity.TeamId,
+                    NationalityId = entity.NationalityId,
+                };
+            }
+        }
     }
 }
