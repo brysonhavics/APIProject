6dbe05966e78f6c52fafd9012ed544c2f7297009
Chris Crowley <cfcrowely42@gmail.com>
2021-09-14 14:58:42 -0400

    added LeagueDetail model

---
+++ FoseBall.Model/LeagueDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoseBall.Model
+{
+    public class LeagueDetail
+    {
+        public int LeagueId { get; set; }
+        public string Name { get; set; }
+        public int NumberOfTeams { get; set; }
+        public string Nation { get; set; }
+    }
+}
