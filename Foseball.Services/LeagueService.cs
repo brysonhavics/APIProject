00ccdfa26bd91f1c07ceae19c1671226a14540e6
Chris Crowley <cfcrowely42@gmail.com>
2021-09-14 11:17:11 -0400

    started League Service

---
+++ Foseball.Services/LeagueService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fluent.Infrastructure.FluentModel;
+using FoseBall.Data;
+
+namespace FoseBall.Model
+{
+    public class LeagueService
+    {
+        private FoseBallDbContext _context = new FoseBallDbContext();
+
+        public bool CreateLeague(LeagueCreate model)
+        {
+            var entity = new League()
+            {
+                Name = model.Name,
+                Nation = model.Nation,
+                NumberOfTeams = model.NumberOfTeams
+            };
+
+            using (var ctx = new FoseBallDbContext())
+            {
+                ctx.Leagues.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
