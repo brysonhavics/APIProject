4a5bc0b5437a3502480184306f96c4f1c0bed2fe
John Mai <Maijohn960@gmail.com>
2021-09-14 14:38:36 -0400

    new controller

---
+++ FoseBall.WebAPI/Controllers/InternationalController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FoseBall.WebAPI
+{
+    public class InternationalController : ApiController
+    {
+        private InternationalService CreateInternationalService()
+        {
+            var InternationalId = Guid.Parse(Id.Coach.Ranking.Name());
+            var InternationalService = new InternationalService(internationalId);
+            return InternationalService;
+        }
+    }
+    
+}
