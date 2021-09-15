1a984decaf4b1ccc0f778d8bb9ec1f4dd841a698
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-15 10:29:03 -0400

    add foreign key to player

--- Foseball.Services/PlayerService.cs
+++ Foseball.Services/PlayerService.cs
@@ -34,7 +34,7 @@ namespace Foseball.Services
                     Name = entity.Name,
                     Number = entity.Number,
                     Position = entity.Position,
-                    TeamId = entity.TeamId,
+                    TeamName = entity.,
                     NationalityId = entity.NationalityId,
                 };
             }
