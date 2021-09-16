11c8ed7c43321d2b9f25dba79748e1f7df49f842
brysonhavics <87788293+brysonhavics@users.noreply.github.com>
2021-09-16 11:42:13 -0400

    change edit team

--- FoseBall.Model/TeamEdit.cs
+++ FoseBall.Model/TeamEdit.cs
@@ -8,7 +8,6 @@ namespace FoseBall.Model
 {
     public class TeamEdit
     {
-        public int TeamId { get; set; }
         public string TeamName { get; set; }
         public int Rank { get; set; }
         public int LeagueId { get; set; }
