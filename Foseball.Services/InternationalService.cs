fe3094194d1835720d6ee45fead6816cb1c5f5aa
John Mai <Maijohn960@gmail.com>
2021-09-16 13:50:45 -0400

    LAtest and greatest

--- Foseball.Services/InternationalService.cs
+++ Foseball.Services/InternationalService.cs
@@ -21,7 +21,7 @@ namespace Foseball.Services
                     .Internationals
                     .Select(e => new InternationalListItem
                     {
-
+                        InternationalId = e.InternationalId,
                         Coach = e.Coach,
                         Ranking = e.Ranking,
                         Name = e.Name,
