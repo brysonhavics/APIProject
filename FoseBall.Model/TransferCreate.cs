d4637ce811351717e19ab6029d3de33739751ccf
Jack McNeil <mcneil.jack.m@gmail.com>
2021-09-20 15:27:17 -0400

    services and table

---
+++ FoseBall.Model/TransferCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoseBall.Model
+{
+    public class TransferCreate
+    {
+        [Required]
+        public double Fee { get; set; }
+        [Display(Name ="Old Team")]
+        [Required]
+        public int TeamId1 { get; set; }
+        [Display(Name ="New Team")]
+        [Required]
+        public int PlayerId { get; set; }
+    }
+}
