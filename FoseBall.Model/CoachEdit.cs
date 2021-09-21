0ef655fc162b10691329a8b81d12d0674b7c1d72
Chris Crowley <cfcrowely42@gmail.com>
2021-09-21 10:16:55 -0400

    added coach edit class

---
+++ FoseBall.Model/CoachEdit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoseBall.Model
+{
+    public class CoachEdit
+    {
+        public int CoachId
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        public string Nationality
+        {
+            get; set;
+        }
+        public int TeamId
+        {
+            get; set;
+        }
+        public int InternationalId
+        {
+            get; set;
+        }
+     }
+}
